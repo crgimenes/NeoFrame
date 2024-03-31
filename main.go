28da5f6fb6abc2e12ebefa2721c3e8189cb22d8b
crgimenes <crg@crg.eti.br>
2024-03-31 12:34:23 -0300

    test send messages to server

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"log"
+	"net"
 	"nf/config"
 	"nf/screen"
+	"os"
 	"path/filepath"
 )
 
@@ -20,14 +24,83 @@ func usage() {
 	flag.PrintDefaults()
 }
 
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	buf := make([]byte, 1024)
+
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				return
+			}
+			fmt.Println("failed to read:", err)
+			return
+		}
+
+		fmt.Println("Recebido:", string(buf[:n]))
+	}
+}
+
+func UDSClient() net.Conn {
+	conn, err := net.Dial("unix", config.CFG.UnixDomainSocket)
+	if err != nil {
+		fmt.Println("failed to dial:", err)
+		os.Exit(1)
+	}
+	return conn
+}
+
+func UDSClientSend(conn net.Conn, msg string) {
+	_, err := conn.Write([]byte(msg))
+	if err != nil {
+		fmt.Println("failed to write:", err)
+		os.Exit(1)
+	}
+}
+
+func UDSCientClose(conn net.Conn) {
+	conn.Close()
+}
+
+func UDSListener() error {
+	_, err := os.Stat(config.CFG.UnixDomainSocket)
+	if err == nil {
+		log.Printf("Unix domain socket %s already exists, remove the file first\n", config.CFG.UnixDomainSocket)
+		return err
+	}
+
+	listener, err := net.Listen("unix", config.CFG.UnixDomainSocket)
+	if err != nil {
+		fmt.Println("failed to listen:", err)
+		os.Exit(1)
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			if !config.CFG.Silent {
+				fmt.Println("failed to accept:", err)
+			}
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+}
+
 func main() {
 	const tmpDir = "/tmp"
 	uds := filepath.Join(tmpDir, "neoframe.sock")
+
+	var cmd string
 
 	flag.BoolVar(&config.CFG.GetScreenInfo, "info", false, "Get screen size")
 	flag.BoolVar(&config.CFG.Silent, "silent", false, "Silent mode")
 	flag.BoolVar(&config.CFG.ServerMode, "server", false, "Server mode")
 	flag.StringVar(&config.CFG.UnixDomainSocket, "uds", uds, "Unix domain socket")
+	flag.StringVar(&cmd, "cmd", "", "Command to send to server")
 
 	flag.Usage = usage
 
@@ -49,6 +122,14 @@ func main() {
 			fmt.Println("Server mode")
 			fmt.Println("Unix domain socket:", config.CFG.UnixDomainSocket)
 		}
+		err := UDSListener()
+		if err != nil {
+			os.Exit(1)
+		}
+	case cmd != "":
+		conn := UDSClient()
+		UDSClientSend(conn, cmd)
+		UDSCientClose(conn)
 	default:
 		usage()
 	}
