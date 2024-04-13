7643a86a8b9eebd26aee06f051e5d0a9f9c8012d
crgimenes <crg@crg.eti.br>
2024-04-13 10:15:29 -0300

    add new flags

--- config/config.go
+++ config/config.go
@@ -4,6 +4,15 @@ type Config struct {
 	GetScreenInfo    bool
 	UnixDomainSocket string
 	ServerMode       bool
+	VisibleWindow    bool
+	WindowTitle      string
+	WindowWidth      int
+	WindowHeight     int
+	WindowX          int
+	WindowY          int
+	WindowBorder     bool
+	WindowBgColor    string
+	RunLuaScript     string
 }
 
 var CFG = &Config{}
